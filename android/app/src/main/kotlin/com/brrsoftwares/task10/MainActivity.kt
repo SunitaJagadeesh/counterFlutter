56fb314dac904e696d8fb2866e419c896f541072
Sunita <sunitavangapandu@gmail.com>
2021-02-15 10:56:45 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/brrsoftwares/task10/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.brrsoftwares.task10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+/* Create a Sample Flutter Application (Counter example one)
+Now Default value of counter should be 100 and when I click on + button the
+counter value should be decreased like 99, 98 ,97 etc */
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: MyHomePage(title: 'Counter App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 100;
+
+  void _decrementCounter() {
+    setState(() {
+
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _decrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.remove),
+      ),
+    );
+  }
+}
